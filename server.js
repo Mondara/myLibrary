7584f0fce4385e669c4adc7511ecfaf6964242f1
Mondara <mondara.t@hotmail.com>
2022-09-14 11:58:13 +1000

    Added Initial Author Routes

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
+const authorsRouter = require("./routes/authors");
 
 const app = express();
 
@@ -19,6 +21,8 @@ app.use(expressLayouts);
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 
 const db = mongoose.connection;
@@ -26,5 +30,6 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorsRouter);
 
 app.listen(process.env.PORT || 3000);
